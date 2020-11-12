19d1c0fd2bf141f359c00306fab56619072827f4
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-11-12 16:39:49 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -60,6 +60,15 @@ DATA_EXPORT = []
 CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
+GDPR = []
+
+START_IMG = os.environ.get('START_IMG', None)
+if START_IMG is None:
+    img = "https://telegra.ph/file/d0ee4291e14794f693e97.jpg"
+else:
+  img = START_IMG    
+    
+
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("tg_bot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
