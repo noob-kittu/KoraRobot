81bbc4a0946e1c947819dbefb1a8591ed0b2662e
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-11-22 18:21:52 +0530

    Update respect.py

--- tg_bot/modules/respect.py
+++ tg_bot/modules/respect.py
@@ -23,7 +23,7 @@ SFW_STRINGS = (
   )
 
 @run_async
-def dark(bot: Bot, update: Update):
+def respect(bot: Bot, update: Update):
     bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
     message = update.effective_message
     if message.reply_to_message:
