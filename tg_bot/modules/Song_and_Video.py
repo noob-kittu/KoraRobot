e49c9f74e4c330ea65664d565109001959ac3abe
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-11-24 07:33:52 +0530

    Update Song_and_Video.py

--- tg_bot/modules/Song_and_Video.py
+++ tg_bot/modules/Song_and_Video.py
@@ -6,7 +6,7 @@ from asyncio import sleep
 from telethon.events import NewMessage
 from telethon.tl.custom import Dialog
 from datetime import datetime as dt
-# from pytz import country_names as c_n, country_timezones as c_tz, timezone as tz
+from pytz import country_names as c_n, country_timezones as c_tz, timezone as tz
 from hachoir.parser import createParser
 import pybase64
 from base64 import b64decode
