5f57c6ee057d839a56d2ecbecbab8d9f96ad82ef
kushal-modi <70826024+kushal-modi@users.noreply.github.com>
2020-11-24 06:52:32 +0530

    Update Song_and_Video.py

--- tg_bot/modules/Song_and_Video.py
+++ tg_bot/modules/Song_and_Video.py
@@ -13,7 +13,7 @@ from base64 import b64decode
 from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo, DocumentAttributeAudio
 from telethon import events
-from tg_bot.events import register
+# from tg_bot.events import register
 from tg_bot.utils import progress
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
