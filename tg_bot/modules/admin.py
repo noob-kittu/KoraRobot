4a61bdcfb8516514071375ccf81728cc91a41b49
Zeref_Dragneel <ZerefDev@gmail.com>
2020-11-25 22:45:33 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -3,7 +3,6 @@ import os
 from typing import Optional, List
 
 import requests
-from telegram.ext import CallbackContext
 from telegram import Message, Chat, Update, Bot, User
 from telegram import ParseMode
 from telegram.error import BadRequest
@@ -20,85 +19,62 @@ from tg_bot.modules.connection import connected
 from tg_bot.modules.translations.strings import tld
 
 @run_async
-@connection_status
 @bot_admin
-@can_promote
 @user_admin
 @loggable
-def promote(update: Update, context: CallbackContext) -> str:
-    bot = context.bot
-    args = context.args
+def promote(bot: Bot, update: Update, args: List[str]) -> str:
+    message = update.effective_message  # type: Optional[Message]
+    user = update.effective_user  # type: Optional[User]
+    chat = update.effective_chat  # type: Optional[Chat]
+    conn = connected(bot, update, chat, user.id)
+    if not conn == False:
+        chatD = dispatcher.bot.getChat(conn)
+    else:
+        chatD = update.effective_chat
+        if chat.type == "private":
+            exit(1)
 
-    message = update.effective_message
-    chat = update.effective_chat
-    user = update.effective_user
 
-    promoter = chat.get_member(user.id)
 
-    if not (promoter.can_promote_members or
-            promoter.status == "creator"):
-        message.reply_text("You don't have the necessary rights to do that!")
-        return
+    if not chatD.get_member(bot.id).can_promote_members:
+        update.effective_message.reply_text("I can't promote/demote people here! "
+                                            "Make sure I'm admin and can appoint new admins.")
+        exit(1)
+
+     
 
     user_id = extract_user(message, args)
-
     if not user_id:
-        message.reply_text(
-            "You don't seem to be referring to a user or the ID specified is incorrect.."
-        )
-        return
+        message.reply_text(tld(chat.id, "You don't seem to be referring to a user."))
+        return ""
 
-    try:
-        user_member = chat.get_member(user_id)
-    except:
-        return
-
+    user_member = chatD.get_member(user_id)
     if user_member.status == 'administrator' or user_member.status == 'creator':
-        message.reply_text(
-            "How am I meant to promote someone that's already an admin?")
-        return
+        message.reply_text(tld(chat.id, "How am I meant to promote someone that's already an admin?"))
+        return ""
 
     if user_id == bot.id:
-        message.reply_text(
-            "I can't promote myself! Get an admin to do it for me.")
-        return
+        message.reply_text(tld(chat.id, "I can't promote myself! Get an admin to do it for me."))
+        return ""
 
     # set same perms as bot - bot can't assign higher perms than itself!
-    bot_member = chat.get_member(bot.id)
+    bot_member = chatD.get_member(bot.id)
 
-    try:
-        bot.promoteChatMember(
-            chat.id,
-            user_id,
-            can_change_info=bot_member.can_change_info,
-            can_post_messages=bot_member.can_post_messages,
-            can_edit_messages=bot_member.can_edit_messages,
-            can_delete_messages=bot_member.can_delete_messages,
-            can_invite_users=bot_member.can_invite_users,
-            # can_promote_members=bot_member.can_promote_members,
-            can_restrict_members=bot_member.can_restrict_members,
-            can_pin_messages=bot_member.can_pin_messages)
-    except BadRequest as err:
-        if err.message == "User_not_mutual_contact":
-            message.reply_text(
-                "I can't promote someone who isn't in the group.")
-        else:
-            message.reply_text("An error occured while promoting.")
-        return
+    bot.promoteChatMember(chatD.id, user_id,
+                          can_change_info=bot_member.can_change_info,
+                          can_post_messages=bot_member.can_post_messages,
+                          can_edit_messages=bot_member.can_edit_messages,
+                          can_delete_messages=bot_member.can_delete_messages,
+                          #can_invite_users=bot_member.can_invite_users,
+                          can_restrict_members=bot_member.can_restrict_members,
+                          can_pin_messages=bot_member.can_pin_messages,
+                          can_promote_members=bot_member.can_promote_members)
 
-    bot.sendMessage(
-        chat.id,
-        f"Sucessfully promoted <b>{user_member.user.first_name or user_id}</b>!",
-        parse_mode=ParseMode.HTML)
-
-    log_message = (
-        f"<b>{html.escape(chat.title)}:</b>\n"
-        f"#PROMOTED\n"
-        f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n"
-        f"<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}"
-    )
-
-    return log_message
+    message.reply_text(tld(chat.id, f"Successfully promoted {mention_html(user_member.user.id, user_member.user.first_name)} in {html.escape(chatD.title)}!"), parse_mode=ParseMode.HTML)
+    return f"<b>{html.escape(chatD.title)}:</b>" \
+            "\n#PROMOTED" \
+           f"\n<b>Admin:</b> {mention_html(user.id, user.first_name)}" \
+           f"\n<b>User:</b> {mention_html(user_member.user.id, user_member.user.first_name)}"
 
 
 @run_async
